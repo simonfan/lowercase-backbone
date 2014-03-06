f33d43fba69113a9d262104b52ead34b7d392ddd
simonfan <simonxd@gmail.com>
2014-03-06 20:59:24 -0300

    added history

--- built/lowercase-backbone.js
+++ built/lowercase-backbone.js
@@ -2,4 +2,4 @@
 //     (c) simonfan
 //     lowercase-backbone is licensed under the MIT terms.
 
-define("lowercase-backbone",["require","exports","module","subject","backbone"],function(e,o,t){var i=e("subject"),n=e("backbone"),r=t.exports=function(e){var o=i(e.prototype);return o.proto("initialize",function(){this.initialize=e.prototype.initialize,e.apply(this,arguments)}),o};return r.model=r(n.Model),r.collection=r(n.Collection),r.view=r(n.View),r.router=r(n.Router),r});
+define("lowercase-backbone",["require","exports","module","subject","backbone"],function(e,o,t){var i=e("subject"),r=e("backbone"),n=t.exports=function(e){var o=i(e.prototype);return o.proto("initialize",function(){this.initialize=e.prototype.initialize,e.apply(this,arguments)}),o};return n.model=n(r.Model),n.collection=n(r.Collection),n.view=n(r.View),n.router=n(r.Router),n.history=r.history,n});
